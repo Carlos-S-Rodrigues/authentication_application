982d9a0da7c4b252922268e1fa52c44b142ab19c
Carlos-S-Rodrigues <carlos.schuenck.rodrigues@gmail.com>
2022-05-25 15:07:05 -0300

    feat: user validation

--- prisma/migrations/20220525175823_create_user/migration.sql
+++ prisma/migrations/20220525175823_create_user/migration.sql
@@ -2,7 +2,7 @@
 CREATE TABLE "users" (
     "id" TEXT NOT NULL PRIMARY KEY,
     "name" TEXT NOT NULL,
-    "gthub_id" INTEGER NOT NULL,
+    "github_id" INTEGER NOT NULL,
     "avatar_url" TEXT NOT NULL,
     "login" TEXT NOT NULL
 );
